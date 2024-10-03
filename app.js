bee20c8bdeedff9870e8205093e360aece35486d
MinkTii <135996684+MinkTii@users.noreply.github.com>
2024-10-02 21:14:53 -0500

    Para mi lulita

---
+++ app.js
@@ -0,0 +1,173 @@
+let anniversary = "2023-11-02";
+let date = new Date(anniversary);
+let dateVal = date.getTime();
+let today = new Date();
+let now = today.getTime();
+let value = now - dateVal;
+let day = Math.floor(value / (1000 * 60 * 60 * 24));
+let month = Math.floor(value / (1000 * 60 * 60 * 24 * 30.4375));
+let year = Math.floor(value / (1000 * 60 * 60 * 24 * 365.25));
+
+console.log(value);
+
+document.getElementById("days").textContent = day.toString();
+document.getElementById("months").textContent = month.toString();
+document.getElementById("years").textContent = year.toString();
+
+
+let musicPlayer = document.querySelector(".music-container");
+let togglePlayer = document.querySelector(".toggle-player");
+
+let trackInfo = document.querySelector(".track-info");
+let trackName = document.querySelector(".trackname");
+let trackArtist = document.querySelector(".trackartist");
+let trackNav = document.querySelector(".track-nav");
+
+let playPauseBtn = document.querySelector(".playpause-track");
+let nextBtn = document.querySelector(".next-track");
+let prevBtn = document.querySelector(".prev-track");
+
+let trackIndex = 0;
+let isPlaying = false;
+let isHidden = true;
+
+let currentTrack = document.createElement("audio");
+let soundBars = document.querySelector(".sound-bars");
+
+togglePlayer.addEventListener("click", function() {
+    isHidden = !isHidden;
+    if(isHidden){
+        musicPlayer.classList.remove("hide");
+        togglePlayer.innerHTML = '<img class="w-[98%]" src="icons/close.svg">';
+        trackInfo.style.transitionDelay = "0.4s";
+        trackNav.style.transitionDelay = "0.4s";
+    } else {
+        musicPlayer.classList.add("hide");
+        togglePlayer.innerHTML = '<img class="w-full" src="icons/plus.svg">';
+        trackInfo.style.transitionDelay = "0s";
+        trackNav.style.transitionDelay = "0s";
+    }
+});
+
+let soundBarsLottie = bodymovin.loadAnimation({
+    container: soundBars,
+    renderer: "svg",
+    loop: true,
+    autoPLay: false,
+    path: "https://lottie.host/9ec12a7e-e429-453a-9f22-a2af1dcb4dca/2zeuy4rwtP.json",
+});
+
+
+let trackList = [
+    {
+        name: "Heather",
+        artist: "Conan Gray",
+        path: "./music/Conan Gray - Heather.mp3",
+    },
+    {
+        name: "Amor Completo",
+        artist: "Mon Laferte",
+        path: "./music/Mon Laferte - Amor Completo.mp3",
+    },
+    {
+        name: "Luna",
+        artist: "Zoé",
+        path: "./music/Zoé - Luna (MTV Unplugged).mp3",
+    },
+    {
+        name: "Akureyri",
+        artist: "Aitana y Sebastián Yatra",
+        path: "./music/Aitana, Sebastián Yatra - Akureyri (Letra).mp3",
+    },
+    {
+        name: "Que la luna sepa",
+        artist: "Solamentedan",
+        path: "./music/Que la luna sepa.mp3",
+    },
+    {
+        name: "Empezaré a vivir",
+        artist: "Danna Paola",
+        path: "./music/Danna Paola - Cuándo empezaré a vivir (De EnredadosVideo Oficial).mp3",
+    },
+    {
+        name: "Love in october",
+        artist: "girl in red",
+        path: "./music/girl in red - we fell in love in october.mp3",
+    },
+    {
+        name: "Veo en ti la luz",
+        artist: "Chayanne y Danna Paola",
+        path: "./music/Chayanne, Danna Paola - Veo en ti la luz (De EnredadosVideo Oficial).mp3",
+    },
+    {
+        name: "Sparks",
+        artist: "Coldplay",
+        path: "./music/Sparks.mp3",
+    },
+    {
+        name: "Compartir",
+        artist: "Carla Morrison",
+        path: "./music/Carla Morrison    Compartir [Letra].mp3",
+    },
+];
+
+// EVENT LISTENERS
+playPauseBtn.addEventListener("click", playPauseTrack);
+nextBtn.addEventListener("click", nextTrack);
+prevBtn.addEventListener("click", prevTrack);
+
+function loadTrack(trackIndex){
+    currentTrack.src = trackList[trackIndex].path;
+    trackName.textContent = trackList[trackIndex].name;
+    trackArtist.textContent = trackList[trackIndex].artist;
+    currentTrack.addEventListener("ended", nextTrack);
+    currentTrack.load();
+}
+
+loadTrack(trackIndex);
+
+function playPauseTrack(){
+    if(isPlaying == false){
+        playTrack();
+    }else{
+        pauseTrack();
+    }
+}
+
+function playTrack(){
+    currentTrack.play();
+    isPlaying = true;
+    playPauseBtn.innerHTML = '<img class="w-8" src="icons/pause.svg">';
+    soundBarsLottie.play();
+}
+
+function pauseTrack(){
+    currentTrack.pause();
+    isPlaying = false;
+    playPauseBtn.innerHTML = '<img class="w-8" src="icons/play.svg">';
+    soundBarsLottie.stop();
+}
+
+function nextTrack(){
+    if(trackIndex < trackList.length - 1){
+        trackIndex += 1;
+        loadTrack(trackIndex);
+        playTrack();
+    }else{
+        trackIndex = 0;
+        loadTrack(trackIndex);
+        playTrack();
+    } 
+}
+
+function prevTrack(){
+    if(trackIndex > 0){
+        trackIndex -= 1;
+        loadTrack(trackIndex);
+        playTrack();
+    }else{
+        trackIndex = trackList.length - 1;
+        loadTrack(trackIndex);
+        playTrack();
+    }
+}
